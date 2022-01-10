dc32a8db8a5a0f8a6a7d59d66738be4bf5e1364e
josephclarke333 <josephclarke333@gmail.com>
2022-01-10 17:13:50 +1000

    Changed back steps per mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -927,7 +927,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 467, 467, 1160, 1040 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 607, 605, 1167.5, 1040 }
 
 /**
  * Default Max Feed Rate (mm/s)
